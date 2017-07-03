c3a2cc3f80861df4af3cbe66ed927ad3a5aa5653
tearth <Tearth@users.noreply.github.com>
2017-07-03 20:04:44 +0200

    Add Client and ClientsManager classes

--- Falcon/Connection.cs
+++ Falcon/Connection.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,19 +10,22 @@ namespace Falcon
 {
     class Connection
     {
+        Socket socket;
+
         public Connection()
         {
-
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
         }
 
         public void StartListening(IPEndPoint endPoint)
         {
-
+            socket.Bind(endPoint);
+            socket.Listen(128);
         }
 
         public void StopListening()
         {
-
+            socket.Close();
         }
     }
 }
