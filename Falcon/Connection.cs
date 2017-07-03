8e344ebc8ff2d9cd5c3eb7a1d17faf6f0e748e0f
tearth <Tearth@users.noreply.github.com>
2017-07-03 19:58:07 +0200

    Add WebSocketServer and Connection classes

---
+++ Falcon/Connection.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Falcon
+{
+    class Connection
+    {
+        public Connection()
+        {
+
+        }
+
+        public void StartListening(IPEndPoint endPoint)
+        {
+
+        }
+
+        public void StopListening()
+        {
+
+        }
+    }
+}
---
+++ Falcon/WebSocketServer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Falcon
+{
+    public class WebSocketServer
+    {
+        Connection server;
+
+        public WebSocketServer()
+        {
+            server = new Connection();
+        }
+
+        public void Open(IPAddress address, short port)
+        {
+            var endPoint = new IPEndPoint(address, port);
+            server.StartListening(endPoint);
+        }
+
+        public void Close()
+        {
+            server.StopListening();
+        }
+    }
+}
