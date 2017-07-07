909094bf1000b0918a52340e68f7a61f8a302034
tearth <Tearth@users.noreply.github.com>
2017-07-07 17:29:03 +0200

    Add WebSocketClient buffer methods

--- Falcon/WebSocketClients/WebSocketClient.cs
+++ Falcon/WebSocketClients/WebSocketClient.cs
@@ -13,10 +13,36 @@ namespace Falcon.WebSocketClients
         public byte[] Buffer { get; set; }
         public bool HandshakeDone { get; set; }
 
+        public int BufferPointer { get; private set; }
+
         public WebSocketClient(String id, int bufferSize)
         {
             this.ID = id;
             this.Buffer = new byte[bufferSize];
+
+            BufferPointer = 0;
+        }
+
+        public bool AddToBuffer(byte[] data)
+        {
+            if (BufferPointer + data.Length > Buffer.Length)
+                return false;
+
+            Array.Copy(data, 0, Buffer, BufferPointer, data.Length);
+            BufferPointer += data.Length;
+            return true;
+        }
+
+        public void RemoveFromBuffer(int length)
+        {
+            Array.Copy(Buffer, length, Buffer, 0, Buffer.Length - length);
+            BufferPointer -= length;
+        }
+
+        public void ClearBuffer()
+        {
+            Array.Clear(Buffer, 0, Buffer.Length);
+            BufferPointer = 0;
         }
     }
 }
