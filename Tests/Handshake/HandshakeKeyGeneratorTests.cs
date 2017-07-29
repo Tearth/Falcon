d2eb657b6b587a65dd2db3391f9da43ed6cf94ae
tearth <Tearth@users.noreply.github.com>
2017-07-29 14:30:37 +0200

    Add tests for HandshakeKeyGenerator

---
+++ Tests/Handshake/HandshakeKeyGeneratorTests.cs
@@ -0,0 +1,33 @@
+﻿using Falcon.Protocol.Handshake;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests.Handshake
+{
+    public class HandshakeKeyGeneratorTests
+    {
+        [Fact]
+        public void Get_ExampleKey_ValidResponse()
+        {
+            var keyGenerator = new HandshakeKeyGenerator();
+
+            var result = keyGenerator.Get("x3JJHMbDL1EzLkh9GBhXDw==");
+
+            Assert.Equal("HSmrc0sMlYUkAGmm5OPpG2HaGWk=", result);
+        }
+
+        [Fact]
+        public void Get_ExampleKey2_ValidResponse()
+        {
+            var keyGenerator = new HandshakeKeyGenerator();
+
+            var result = keyGenerator.Get("dGhlIHNhbXBsZSBub25jZQ==");
+
+            Assert.Equal("s3pPLMBiTxaQ9kYGzzhZRbK+xOo=", result);
+        }
+    }
+}
