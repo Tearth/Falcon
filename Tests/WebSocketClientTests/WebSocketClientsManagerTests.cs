ee145d75bdc0963f61bc4d5c7214f0e8a4cc193e
tearth <Tearth@users.noreply.github.com>
2017-07-30 17:27:44 +0200

    Add tests for WebSocketClientsManager

--- Tests/WebSocketClientTests/WebSocketClientsManagerTests.cs
+++ Tests/WebSocketClientTests/WebSocketClientsManagerTests.cs
@@ -10,6 +10,47 @@ namespace Tests.WebSocketClientTests
 {
     public class WebSocketClientsManagerTests
     {
-        
+        [Fact]
+        public void Add_ValidClient_ClientExists()
+        {
+            var clientsManager = new WebSocketClientsManager();
+            var client = new WebSocketClient(null, 16);
+
+            clientsManager.Add(client);
+
+            Assert.NotNull(clientsManager.GetByID(client.ID));
+        }
+
+        [Fact]
+        public void Remove_ValidClient_ClientNotExists()
+        {
+            var clientsManager = new WebSocketClientsManager();
+            var client = new WebSocketClient(null, 16);
+
+            clientsManager.Add(client);
+            clientsManager.Remove(client);
+
+            Assert.Null(clientsManager.GetByID(client.ID));
+        }
+
+        [Fact]
+        public void Exists_ValidClient_TrueResult()
+        {
+            var clientsManager = new WebSocketClientsManager();
+            var client = new WebSocketClient(null, 16);
+
+            clientsManager.Add(client);
+
+            Assert.True(clientsManager.Exists(client.ID));
+        }
+
+        [Fact]
+        public void Exists_InvalidClient_FalseResult()
+        {
+            var clientsManager = new WebSocketClientsManager();
+            var client = new WebSocketClient(null, 16);
+
+            Assert.False(clientsManager.Exists(client.ID));
+        }
     }
 }
