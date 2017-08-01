8ecef80c439185429c60e8be242408933ce9a430
tearth <Tearth@users.noreply.github.com>
2017-08-01 16:59:46 +0200

    Add tests to WebSocketServer

---
+++ Tests/FakeServerListener.cs
@@ -0,0 +1,52 @@
+﻿using Falcon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Falcon.SocketServices.EventArguments;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Tests
+{
+    public class FakeServerListener : IServerListener
+    {
+        public EServerState ServerState { get; private set; }
+
+        public event EventHandler<ConnectedEventArgs> ClientConnected;
+        public event EventHandler<DisconnectedEventArgs> ClientDisconnected;
+        public event EventHandler<DataReceivedEventArgs> DataReceived;
+        public event EventHandler<DataSentEventArgs> DataSent;
+
+        public FakeServerListener()
+        {
+            ServerState = EServerState.Closed;
+        }
+
+        public void Start(IPEndPoint endpoint)
+        {
+            ServerState = EServerState.Working;
+        }
+
+        public void Stop()
+        {
+            ServerState = EServerState.Closed;
+        }
+
+        public void CloseConnection(Socket socket)
+        {
+            
+        }
+
+        public void CloseConnection(Socket socket, Exception ex)
+        {
+
+        }
+
+        public void Send(Socket socket, byte[] data)
+        {
+
+        }
+    }
+}
---
+++ Tests/WebSocketServerTests.cs
@@ -0,0 +1,63 @@
+﻿using Falcon;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Xml.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Falcon.Exceptions;
+
+namespace Tests
+{
+    public class WebSocketServerTests
+    {
+        [Fact]
+        public void Start_OneCall_NoException()
+        {
+            var fakeServerListener = new FakeServerListener();
+            var webSocketServer = new WebSocketServer(1024, fakeServerListener);
+            
+            var ex = Record.Exception(() => webSocketServer.Start(IPAddress.Any, 12345));
+
+            Assert.Null(ex);
+        }
+
+        [Fact]
+        public void Start_DoubleCall_ServerAlreadyWorkingException()
+        {
+            var fakeServerListener = new FakeServerListener();
+            var webSocketServer = new WebSocketServer(1024, fakeServerListener);
+
+            webSocketServer.Start(IPAddress.Any, 12345);
+
+            Assert.Throws<ServerAlreadyWorkingException>(() => webSocketServer.Start(IPAddress.Any, 12345));
+        }
+
+        [Fact]
+        public void Stop_OneCall_NoException()
+        {
+            var fakeServerListener = new FakeServerListener();
+            var webSocketServer = new WebSocketServer(1024, fakeServerListener);
+            
+            webSocketServer.Start(IPAddress.Any, 12345);
+            var ex = Record.Exception(() => webSocketServer.Stop());
+
+            Assert.Null(ex);
+        }
+
+        [Fact]
+        public void Stop_DoubleCall_ServerAlreadyClosedException()
+        {
+            var fakeServerListener = new FakeServerListener();
+            var webSocketServer = new WebSocketServer(1024, fakeServerListener);
+
+            webSocketServer.Start(IPAddress.Any, 12345);
+            webSocketServer.Stop();
+
+            Assert.Throws<ServerAlreadyClosedException>(() => webSocketServer.Stop());
+        }
+    }
+}
